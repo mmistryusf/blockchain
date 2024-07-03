05403db4785a7ef4e0a73ba20317b10c9612a044
mmistryusf <157323727+mmistryusf@users.noreply.github.com>
2024-07-03 18:54:36 -0400

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -1,10 +1,14 @@
 import requests
 import json
+import ipfshttpclient
 
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
-
+	json_data = json.dumps(data)
+	client = ipfshttpclient.connect()
+	cid = client.add_str(json_data)
+	return cid
 	return cid
 
 def get_from_ipfs(cid,content_type="json"):
